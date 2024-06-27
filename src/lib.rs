001e6928dd7440407fdf44b314cec6db2dab9599
Raymond Nambaale <nambaaler@gmail.com>
2024-06-27 16:57:19 +0300

    Fixes readme links, removes dead code

--- src/lib.rs
+++ src/lib.rs
@@ -8,18 +8,3 @@ pub mod wallets;
 
 pub use crate::core::*;
 pub use crate::eversend::*;
-
-// pub fn add(left: usize, right: usize) -> usize {
-//     left + right
-// }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::*;
-
-//     #[test]
-//     fn it_works() {
-//         let result = add(2, 2);
-//         assert_eq!(result, 4);
-//     }
-// }
