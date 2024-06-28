89c629acc102a064d4c39de3572e6d20cd83e15e
Raymond Nambaale <nambaaler@gmail.com>
2024-06-28 18:03:48 +0300

    Implements fetch single wallet feature (#5)

--- src/wallets/operations.rs
+++ src/wallets/operations.rs
@@ -1,3 +1,5 @@
+mod get_wallet;
 mod get_wallets;
 
+pub use get_wallet::*;
 pub use get_wallets::*;
--- src/wallets/types/wallet.rs
+++ src/wallets/types/wallet.rs
@@ -1,3 +1,4 @@
+use std::fmt::Display;
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
@@ -19,4 +20,26 @@ pub struct Wallet {
 
     #[serde(rename = "isMain")]
     pub is_main: bool,
+}
+
+/// The ID of a [`Wallet`].
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Serialize, Deserialize)]
+pub struct WalletId(String);
+
+impl Display for WalletId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<String> for WalletId {
+    fn from(value: String) -> Self {
+        Self(value)
+    }
+}
+
+impl From<&str> for WalletId {
+    fn from(value: &str) -> Self {
+        Self(value.to_string())
+    }
 }
