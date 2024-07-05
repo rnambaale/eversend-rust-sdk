a3b115ec7416eeb8275be68549621769fb512c47
Raymond Nambaale <nambaaler@gmail.com>
2024-07-05 09:01:46 +0300

    Edit a Beneficiary (#18)
    * Edit a Beneficiary
    
    * Update docs

--- src/beneficiaries/operations.rs
+++ src/beneficiaries/operations.rs
@@ -1,7 +1,9 @@
 mod create_beneficiaries;
+mod edit_beneficiary;
 mod get_beneficiaries;
 mod get_beneficiary;
 
 pub use create_beneficiaries::*;
+pub use edit_beneficiary::*;
 pub use get_beneficiaries::*;
 pub use get_beneficiary::*;
