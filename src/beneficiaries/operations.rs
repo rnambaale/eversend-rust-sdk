dafc18d0d725031cd27b2ddf2b0062fd8395c452
Raymond Nambaale <nambaaler@gmail.com>
2024-07-06 07:17:21 +0300

    Delete a beneficiary (#19)

--- src/beneficiaries/operations.rs
+++ src/beneficiaries/operations.rs
@@ -1,9 +1,11 @@
 mod create_beneficiaries;
+mod delete_beneficiary;
 mod edit_beneficiary;
 mod get_beneficiaries;
 mod get_beneficiary;
 
 pub use create_beneficiaries::*;
+pub use delete_beneficiary::*;
 pub use edit_beneficiary::*;
 pub use get_beneficiaries::*;
 pub use get_beneficiary::*;
