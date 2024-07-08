1329efdbf47058396fcc4ff2a0e2420b62b87dec
Raymond Nambaale <nambaaler@gmail.com>
2024-07-08 09:19:21 +0300

    Create eversend payout transaction

--- src/payouts/operations.rs
+++ src/payouts/operations.rs
@@ -1,6 +1,7 @@
 mod create_bank_payout_transaction;
 mod create_beneficiary_payout_transaction;
 mod create_eversend_payout_quotation;
+mod create_eversend_payout_transaction;
 mod create_momo_and_bank_payout_quotation;
 mod create_momo_payout_transaction;
 mod get_delivery_banks;
@@ -9,6 +10,7 @@ mod get_delivery_countries;
 pub use create_bank_payout_transaction::*;
 pub use create_beneficiary_payout_transaction::*;
 pub use create_eversend_payout_quotation::*;
+pub use create_eversend_payout_transaction::*;
 pub use create_momo_and_bank_payout_quotation::*;
 pub use create_momo_payout_transaction::*;
 pub use get_delivery_banks::*;
