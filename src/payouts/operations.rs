349949fdaa43a5fa3922d1441d70ec7143ea13cb
Raymond Nambaale <nambaaler@gmail.com>
2024-07-13 11:26:30 +0300

    Create beneficiary payout transaction (#29)

--- src/payouts/operations.rs
+++ src/payouts/operations.rs
@@ -1,10 +1,12 @@
 mod create_bank_payout_transaction;
+mod create_beneficiary_payout_transaction;
 mod create_momo_and_bank_payout_quotation;
 mod create_momo_payout_transaction;
 mod get_delivery_banks;
 mod get_delivery_countries;
 
 pub use create_bank_payout_transaction::*;
+pub use create_beneficiary_payout_transaction::*;
 pub use create_momo_and_bank_payout_quotation::*;
 pub use create_momo_payout_transaction::*;
 pub use get_delivery_banks::*;
